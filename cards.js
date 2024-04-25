39ba51bef50c113b3ec7afbee9aa2a1a32a634ba
Chris <Christopherjorge47@icloud.com>
2024-04-25 14:24:50 -0400

    Modified model names and created NewDeck object in cards.js

---
+++ cards.js
@@ -0,0 +1,221 @@
+const NewDeck = {
+    '2Club': {
+        value: 2,
+        url: './models/2Club.svg.png',
+        leftInDeck: 12
+    },
+    '2Diamond': {
+        value: 2,
+        url: './models/2Diamond.svg.png',
+        leftInDeck: 12
+    },
+    '2Spade': {
+        value: 2,
+        url: './models/2Spade.svg.png',
+        leftInDeck: 12
+    },
+    '2Heart': {
+        value: 2,
+        url: './models/2Heart.svg.png',
+        leftInDeck: 12
+    },
+    '3Club': {
+        value: 3,
+        url: './models/3Club.svg.png',
+        leftInDeck: 12
+    },
+    '3Diamond': {
+        value: 3,
+        url: './models/3Diamond.svg.png',
+        leftInDeck: 12
+    },
+    '3Spade': {
+        value: 3,
+        url: './models/3Spade.svg.png',
+        leftInDeck: 12
+    },
+    '3Heart': {
+        value: 3,
+        url: './models/3Heart.svg.png',
+        leftInDeck: 12
+    },
+    '4Club': {
+        value: 4,
+        url: './models/4Club.svg.png',
+        leftInDeck: 12
+    },
+    '4Diamond': {
+        value: 4,
+        url: './models/4Diamond.svg.png',
+        leftInDeck: 12
+    },
+    '4Spade': {
+        value: 4,
+        url: './models/4Spade.svg.png',
+        leftInDeck: 12
+    },
+    '4Heart': {
+        value: 4,
+        url: './models/4Heart.svg.png',
+        leftInDeck: 12
+    },
+    '5Club': {
+        value: 5,
+        url: './models/5Club.svg.png',
+        leftInDeck: 12
+    },
+    '5Diamond': {
+        value: 5,
+        url: './models/5Diamond.svg.png',
+        leftInDeck: 12
+    },
+    '5Spade': {
+        value: 5,
+        url: './models/5Spade.svg.png',
+        leftInDeck: 12
+    },
+    '5Heart': {
+        value: 5,
+        url: './models/5Heart.svg.png',
+        leftInDeck: 12
+    },
+    '6Club': {
+        value: 6,
+        url: './models/6Club.svg.png',
+        leftInDeck: 12
+    },
+    '6Diamond': {
+        value: 6,
+        url: './models/6Diamond.svg.png',
+        leftInDeck: 12
+    },
+    '6Spade': {
+        value: 6,
+        url: './models/6Spade.svg.png',
+        leftInDeck: 12
+    },
+    '6Heart': {
+        value: 6,
+        url: './models/6Heart.svg.png',
+        leftInDeck: 12
+    },
+    '7Club': {
+        value: 7,
+        url: './models/7Club.svg.png',
+        leftInDeck: 12
+    },
+    '7Diamond': {
+        value: 7,
+        url: './models/7Diamond.svg.png',
+        leftInDeck: 12
+    },
+    '7Spade': {
+        value: 7,
+        url: './models/7Spade.svg.png',
+        leftInDeck: 12
+    },
+    '7Heart': {
+        value: 7,
+        url: './models/7Heart.svg.png',
+        leftInDeck: 12
+    },
+    '8Club': {
+        value: 8,
+        url: './models/8Club.svg.png',
+        leftInDeck: 12
+    },
+    '8Diamond': {
+        value: 8,
+        url: './models/5Diamond.svg.png',
+        leftInDeck: 12
+    },
+    '8Spade': {
+        value: 8,
+        url: './models/5Spade.svg.png',
+        leftInDeck: 12
+    },
+    '8Heart': {
+        value: 8,
+        url: './models/5Heart.svg.png',
+        leftInDeck: 12
+    },
+    '9Club': {
+        value: 9,
+        url: './models/9Club.svg.png',
+        leftInDeck: 12
+    },
+    '9Diamond': {
+        value: 9,
+        url: './models/9Diamond.svg.png',
+        leftInDeck: 12
+    },
+    '9Spade': {
+        value: 9,
+        url: './models/9Spade.svg.png',
+        leftInDeck: 12
+    },
+    '9Heart': {
+        value: 9,
+        url: './models/9Heart.svg.png',
+        leftInDeck: 12
+    },
+    '10Club': {
+        value: 10,
+        url: './models/10Club.svg.png',
+        leftInDeck: 12
+    },
+    '10Diamond': {
+        value: 10,
+        url: './models/10Diamond.svg.png',
+        leftInDeck: 12
+    },
+    '10Spade': {
+        value: 10,
+        url: './models/10Spade.svg.png',
+        leftInDeck: 12
+    },
+    '10Heart': {
+        value: 10,
+        url: './models/10Heart.svg.png',
+        leftInDeck: 12
+    },
+    'AceClub': {
+        value: [1,11],
+        url: './models/AceClub.svg.png',
+        leftInDeck: 6
+    },
+    'AceDiamond': {
+        value: [1,11],
+        url: './models/AceDiamond.svg.png',
+        leftInDeck: 6
+    },
+    'AceSpade': {
+        value: [1,11],
+        url: './models/AceSpade.svg.png',
+        leftInDeck: 6
+    },
+    'AceHeart': {
+        value: [1,11],
+        url: './models/AceHeart.svg.png',
+        leftInDeck: 6
+    },
+    'Jack': {
+        value: 10,
+        url: './models/Jack.svg.png',
+        leftInDeck: 24
+    },
+    'King': {
+        value: 10,
+        url: './models/King.svg.png',
+        leftInDeck: 24
+    },
+    'Queen': {
+        value: 10,
+        url: './models/Queen.svg.png',
+        leftInDeck: 24
+    }
+    
+    
+}
+
+module.exports = NewDeck
