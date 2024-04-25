7ee12765d5a17c6ae3150101e135fec3eb407cfe
Chris <Christopherjorge47@icloud.com>
2024-04-25 14:29:16 -0400

    Added options array

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 let deck = require('./cards')
+let options = 
+['2Club', '2Diamond', '2Heart', '2Spade',
+'3Club', '3Diamond', '3Heart', '3Spade',
+'4Club', '4Diamond', '4Heart', '4Spade',
+'5Club', '5Diamond', '5Heart', '5Spade',
+'6Club', '6Diamond', '6Heart', '6Spade',
+'7Club', '7Diamond', '7Heart', '7Spade',
+'8Club', '8Diamond', '8Heart', '8Spade',
+'9Club', '9Diamond', '9Heart', '9Spade',
+'10Club', '10Diamond', '10Heart', '10Spade',
+'AceClub', 'AceDiamond', 'AceHeart', 'AceSpade',
+'Jack', 'Queen', 'King']
+
